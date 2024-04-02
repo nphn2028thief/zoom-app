c41911f0bf63fea83b4e13f67028594b39603ee5
Nguyen Phan Hoai Nhan <nphn2082thief@gmail.com>
2024-04-02 17:40:32 +0700

    Implement schedule meeting modal when create schedule meeting and create success schedule meeting

--- src/components/features/MeetingList/index.tsx
+++ src/components/features/MeetingList/index.tsx
@@ -1,19 +1,22 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { useCallback, useState } from "react";
 import { useUser } from "@clerk/nextjs";
 import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
+import ReactDatePicker from "react-datepicker";
 
 import MeetingItem from "./MeetingItem";
 import MeetingModal from "@/components/modals/MeetingModal";
 import { useToast } from "@/components/ui/use-toast";
+import { Textarea } from "@/components/ui/textarea";
 import { meetingList } from "@/constants/meeting";
 import { EPath } from "@/constants/path";
 import { TMeetingState } from "@/types/meeting";
 
 const MeetingList = () => {
   const router = useRouter();
+  const pathname = usePathname();
 
   const [meetingState, setMeetingState] = useState<TMeetingState>();
   const [values, setValues] = useState({
@@ -97,6 +100,63 @@ const MeetingList = () => {
         onSubmit={handleCreateMeeting}
         onClose={onClose}
       />
+
+      {callDetails ? (
+        <MeetingModal
+          isOpen={meetingState === "isSCheduleMeeting"}
+          title="Meeting created"
+          imgUrl="/icons/checked.svg"
+          buttonIcon="/icons/copy.svg"
+          buttonText="copy meeting link"
+          className="text-center"
+          onSubmit={() => {
+            const meetingLink = `${window.location.origin}${EPath.MEETING}/${callDetails.id}`;
+            navigator.clipboard.writeText(meetingLink);
+            toast({ title: "Link copied!" });
+          }}
+          onClose={onClose}
+        />
+      ) : (
+        <MeetingModal
+          isOpen={meetingState === "isSCheduleMeeting"}
+          title="Create meeting"
+          onSubmit={handleCreateMeeting}
+          onClose={onClose}
+        >
+          <form action="" className="flex flex-col gap-2.5">
+            <div className="flex flex-col gap-2 5">
+              <label
+                htmlFor="description"
+                className="text-sky-2 leading-[22px]"
+              >
+                Add a description
+              </label>
+              <Textarea
+                id="description"
+                className="bg-dark-3 border-none focus-visible:ring-0 focus-visible:ring-offset-0"
+                onChange={(e) =>
+                  setValues({ ...values, description: e.target.value })
+                }
+              />
+            </div>
+            <div className="flex flex-col gap-2 5">
+              <label htmlFor="datetime" className="text-sky-2 leading-[22px]">
+                Select Date and Time
+              </label>
+              <ReactDatePicker
+                selected={values.dateTime}
+                showTimeSelect
+                timeFormat="HH:mm"
+                timeIntervals={15} // 15 minues
+                timeCaption="time"
+                dateFormat="MMMM d, yyyy h:mm aa"
+                className="w-full p-2 bg-dark-3 rounded focus:outline-none"
+                onChange={(date) => setValues({ ...values, dateTime: date! })}
+              />
+            </div>
+          </form>
+        </MeetingModal>
+      )}
     </section>
   );
 };
