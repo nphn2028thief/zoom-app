02e8a4f5a5ac33f25a36d436a6997c089aaf51b6
Nguyen Phan Hoai Nhan <nphn2082thief@gmail.com>
2024-04-02 12:44:14 +0700

    Add missing code in app root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Toaster } from "@/components/ui/toaster";
 
 import { ClerkProvider } from "@clerk/nextjs";
+
+import { Toaster } from "@/components/ui/toaster";
 
 import "./globals.css";
 
